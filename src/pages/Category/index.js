327ee4ccbbe967cb2a4465bd910ece1b967c86c1
MiguelS007 <miguelsodre007@gmail.com>
2020-07-28 18:01:57 -0300

    Starting form on the category screen

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -7,8 +7,19 @@ function CategoryRegister() {
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
 
+      <form>
+        <label>
+          Nome da Categoria:
+          <input type="text" />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
       <Link to="/">
-       Ir para home
+        Ir para home
       </Link>
     </PageDefault>
   );
